247679160e1002be541e72d4f37fe46f577a1001
Cristian vaghetti <vaghetticristian@gmail.com>
2022-03-15 23:31:06 -0300

    criação das tabelas e relacionamentos

---
+++ Projeto-Camara_Vereadores-Capao_do_Leao/database/migrations/2022_03_16_020650_create_materials_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materiais', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao', 50);
+            $table->float('valor', 8, 2)->default(1);
+            $table->integer('quantidade')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materiais');
+    }
+}
---
+++ Projeto-Camara_Vereadores-Capao_do_Leao/database/migrations/2022_03_16_020728_create_fornecedors_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFornecedorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fornecedores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        Schema::table('materiais', function (Blueprint $table) {
+            $table->unsignedBigInteger('fornecedor_id');
+            $table->foreign('fornecedor_id')->references('id')->on('fornecdores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('materiais', function (Blueprint $table){
+            $table->dropForeign('materiais_fornecedor_id_foreign');
+
+            $table->dropColumn('fornecedor_id');
+        });
+
+        Schema::dropIfExists('fornecedores');
+    }
+}
